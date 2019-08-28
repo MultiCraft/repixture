482c05682ac158f11ac957496351d852088bf11b
Wuzzy <Wuzzy2@mail.ru>
2019-08-28 18:03:54 +0200

    Add stubs for waypoint functions

--- mods/nav/init.lua
+++ mods/nav/init.lua
@@ -5,6 +5,7 @@
 
 nav = {}
 
+dofile(minetest.get_modpath("nav").."/waypoints.lua") -- TODO: Waypoint implenentation is a stub
 dofile(minetest.get_modpath("nav").."/map.lua")
 dofile(minetest.get_modpath("nav").."/compass.lua")
 
---
+++ mods/nav/waypoints.lua
@@ -0,0 +1,10 @@
+-- Waypoint handling is a stub at the moment.
+-- TODO: Implement waypoints
+
+function nav.add_waypoint(pos, name, label, isinfo, w_type)
+	-- This is a stub
+end
+
+function nav.remove_waypoint(name)
+	-- This is a stub
+end
