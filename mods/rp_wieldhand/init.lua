bf097bf4c37962d349e5da35ba591b7406475fad
Wuzzy <Wuzzy@disroot.org>
2022-04-23 06:44:09 +0200

    Rename wieldhand → rp_wieldhand

--- mods/rp_wieldhand/init.lua
+++ mods/rp_wieldhand/init.lua
@@ -1,7 +1,7 @@
 wieldhand = {}
 
 for h=0, 9 do
-	minetest.register_item("wieldhand:hand_"..h, {
+	minetest.register_item("rp_wieldhand:hand_"..h, {
 		type = "none",
 		wield_image = "wieldhand_"..h..".png",
 		wield_scale = {x=1.0,y=1.0,z=3.0},
@@ -10,6 +10,6 @@ end
 
 function wieldhand.set_hand(player, skin_tone)
 	local inv = player:get_inventory()
-	local hand = ItemStack("wieldhand:hand_"..skin_tone)
+	local hand = ItemStack("rp_wieldhand:hand_"..skin_tone)
 	inv:set_stack("hand", 1, hand)
 end
