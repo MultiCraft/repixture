f44763565172d45716d50e11e1654717a9f1db54
Wuzzy <Wuzzy@disroot.org>
2022-04-23 08:10:27 +0200

    Add missing aliases of renaming orgy

--- mods/rp_wieldhand/init.lua
+++ mods/rp_wieldhand/init.lua
@@ -13,3 +13,8 @@ function wieldhand.set_hand(player, skin_tone)
 	local hand = ItemStack("rp_wieldhand:hand_"..skin_tone)
 	inv:set_stack("hand", 1, hand)
 end
+
+-- Legacy aliases
+for h=0, 9 do
+	minetest.register_alias("wieldhand:hand_"..h, "rp_wieldhand:hand_"..h)
+end
