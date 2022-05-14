92183e279db3b68214afadf78fce6da780c0a54e
Wuzzy <Wuzzy@disroot.org>
2022-05-14 13:19:30 +0200

    Generate wheat and cotton in map

--- mods/rp_farming/mapgen.lua
+++ mods/rp_farming/mapgen.lua
@@ -11,7 +11,7 @@ minetest.register_decoration(
       fill_ratio = 0.008,
       biomes = {"Wilderness"},
       decoration = {"rp_farming:wheat_4"},
-      y_min = 0,
+      y_min = 1,
       y_max = 32000,
 })
 
@@ -20,21 +20,29 @@ minetest.register_decoration(
       deco_type = "simple",
       place_on = "rp_default:dirt_with_grass",
       sidelen = 16,
-      fill_ratio = 0.006,
-      biomes = {"Grassland", "Savanna"},
+      fill_ratio = 0.001,
+      biomes = {"Grassland"},
       decoration = {"rp_farming:wheat_4"},
-      y_min = 0,
+      y_min = 1,
       y_max = 32000,
+      noise_params = {
+          seed = 13,
+	  octaves = 2,
+	  scale = 0.001,
+	  offset = 0.0,
+	  spread = { x = 50, y = 50, z = 50 },
+      },
 })
 
 minetest.register_decoration(
    {
       deco_type = "simple",
-      place_on = "rp_default:sand",
+      place_on = {"rp_default:sand", "rp_default:dirt_with_dry_grass"},
       sidelen = 16,
       fill_ratio = 0.004,
-      biomes = {"Desert"},
+      biomes = {"Desert", "Savanna"},
       decoration = {"rp_farming:cotton_4"},
-      y_min = 0,
+      y_min = 1,
       y_max = 32000,
 })
+
