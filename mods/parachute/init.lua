9a5fdf80e663b11f03410b9078f8bc889848a8ef
Wuzzy <Wuzzy@disroot.org>
2022-05-20 17:14:39 +0200

    Respawn parachute to player who left with it

--- mods/parachute/init.lua
+++ mods/parachute/init.lua
@@ -20,12 +20,17 @@ end
 -- Tries to spawn a parachute entity and attaches it to player.
 -- Will fail if player is already attached to something
 -- or if player is too close to the ground.
+-- * `player`: Player to open the parachute for
+-- * `play_sound`: If true, will play a sound for opening the parachute (default: true)
 -- Returns true on success or
 -- false, <failure_reason> on failure.
 -- * <failure_reason> = "already_attached" if player was attached
 -- * <failure_reason> = "on_ground" if player already on ground
-local function open_parachute_for_player(player)
+local function open_parachute_for_player(player, play_sound)
    local name = player:get_player_name()
+   if play_sound == nil then
+      play_sound = true
+   end
 
    local pos = player:get_pos()
 
@@ -35,19 +40,25 @@ local function open_parachute_for_player(player)
       return false, "already_attached"
    end
 
-   if on.name == "air" then
+   if on.name == "air" or on.name == "ignore" then
       -- Spawn parachute
-      pos.y = pos.y + 3
+      local ppos = vector.new(pos.x, pos.y + 3, pos.z)
 
-      local obj = minetest.add_entity(pos, "parachute:entity")
-      minetest.sound_play({name="parachute_open", pos=pos}, {gain=0.5}, true)
+      local obj = minetest.add_entity(ppos, "parachute:entity")
+      if play_sound then
+         minetest.sound_play({name="parachute_open", pos=ppos}, {gain=0.5}, true)
+      end
 
-      obj:set_velocity(
-         {
-            x = 0,
-            y = math.min(0, player:get_velocity().y),
-            z = 0
-      })
+      if on.name ~= "ignore" then
+         obj:set_velocity(
+            {
+               x = 0,
+               y = math.min(0, player:get_velocity().y),
+               z = 0
+         })
+      else
+	 obj:set_velocity(vector.zero())
+      end
 
       player:set_attach(obj, "", {x = 0, y = -8, z = 0}, {x = 0, y = 0, z = 0}, true)
 
@@ -56,7 +67,15 @@ local function open_parachute_for_player(player)
       local lua = obj:get_luaentity()
       lua.attached = name
 
+      local in_node = minetest.get_node(pos)
+      lua.ignore_mode = in_node.name == "ignore"
+
       rp_player.player_attached[name] = true
+
+      local meta = player:get_meta()
+      -- Save parachute state in player meta. Used to re-open the parachute when
+      -- leaving the server and then re-joining.
+      meta:set_int("parachute:active", 1)
 
       minetest.log("action", "[parachute] "..name.." opens a parachute at "..minetest.pos_to_string(obj:get_pos(), 1))
       return true
@@ -105,6 +124,7 @@ minetest.register_entity(
 
       attached = nil,
       start_y = nil,
+      ignore_mode = false,
 
       on_activate = function(self, staticdata, dtime_s)
          if dtime_s == 0 then
@@ -115,6 +135,9 @@ minetest.register_entity(
       on_step = function(self, dtime)
          local pos = self.object:get_pos()
          local under = minetest.get_node({x = pos.x, y = pos.y - 1, z = pos.z})
+	 if under.name ~= "ignore" then
+            self.ignore_mode = false
+	 end
 
          if self.attached ~= nil then
             local player = minetest.get_player_by_name(self.attached)
@@ -161,20 +184,27 @@ minetest.register_entity(
 
             accel.y = accel.y + air_physics(vel.y) * 0.25
 
-            self.object:set_acceleration(accel)
+	    if under.name ~= "ignore" then
+               self.object:set_acceleration(accel)
+            else
+               self.object:set_acceleration(vector.zero())
+               self.object:set_velocity(vector.zero())
+            end
 
-            if under.name ~= "air" then
+            if under.name ~= "air" and (self.ignore_mode == false or under.name ~= "ignore") then
                rp_player.player_attached[self.attached] = false
             end
          end
 
-         if under.name ~= "air" then
+         if under.name ~= "air" and (self.ignore_mode == false or under.name ~= "ignore") then
             local player
             if self.attached ~= nil then
                rp_player.player_attached[self.attached] = false
 
                player = minetest.get_player_by_name(self.attached)
                if player then
+                  local meta = player:get_meta()
+                  meta:set_int("parachute:active", 0)
                   if self.start_y ~= nil and self.start_y - self.object:get_pos().y > 100 then
                      achievements.trigger_achievement(player, "sky_diver")
                   end
@@ -205,6 +235,19 @@ minetest.register_entity(
          end
       end,
 })
+
+minetest.register_on_joinplayer(function(player)
+   local meta = player:get_meta()
+   -- Re-open parachute when player left the server
+   -- with the parachute still active.
+   -- Important to prevent the player falling to their doom.
+   -- This isn't perfect as the old velocity isn't preserved but better
+   -- better than nothing.
+   if meta:get_int("parachute:active") == 1 then
+      minetest.log("action", "[parachute] Trying to open parachute for reconnected player "..player:get_player_name().." ...")
+      open_parachute_for_player(player, false)
+   end
+end)
 
 -- Crafting
 
