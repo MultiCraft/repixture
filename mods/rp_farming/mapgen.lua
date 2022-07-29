7d8ce9d2fbb11d789b4e7804f925135d95e53ce0
Wuzzy <Wuzzy@disroot.org>
2022-07-29 18:51:05 +0200

    Add potato/asparagus/carrot to mapgen

--- mods/rp_farming/mapgen.lua
+++ mods/rp_farming/mapgen.lua
@@ -20,6 +20,42 @@ minetest.register_decoration(
       deco_type = "simple",
       place_on = "rp_default:dirt_with_grass",
       sidelen = 16,
+      fill_ratio = 0.0005,
+      biomes = {"Grove", "Shrubbery", "Baby Poplar Plains"},
+      decoration = {"rp_farming:potato_4"},
+      y_min = 2,
+      y_max = 32000,
+})
+
+minetest.register_decoration(
+   {
+      deco_type = "simple",
+      place_on = {"rp_default:dirt_with_swamp_grass", "rp_default:swamp_dirt"},
+      sidelen = 16,
+      fill_ratio = 0.0004,
+      biomes = {"Mixed Swamp", "Mixed Swamp Highland", "Swamp Meadow", "Swamp Meadow Highland", "Papyrus Swamp"},
+      decoration = {"rp_farming:asparagus_4"},
+      y_min = 1,
+      y_max = 32000,
+})
+
+minetest.register_decoration(
+   {
+      deco_type = "simple",
+      place_on = {"rp_default:dry_dirt"},
+      sidelen = 16,
+      fill_ratio = 0.0001,
+      biomes = {"Wooded Dryland"},
+      decoration = {"rp_farming:carrot_4"},
+      y_min = 5,
+      y_max = 32000,
+})
+
+minetest.register_decoration(
+   {
+      deco_type = "simple",
+      place_on = "rp_default:dirt_with_grass",
+      sidelen = 16,
       fill_ratio = 0.002,
       biomes = {"Dense Grassland"},
       decoration = {"rp_farming:wheat_4"},
