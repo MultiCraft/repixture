5b5677e55a52cdfb967de76fcb8f1777ea248870
Wuzzy <Wuzzy@disroot.org>
2023-04-15 15:49:15 +0200

    Add pointed_thing arg to rp_bed on_rightclick call

--- mods/rp_bed/init.lua
+++ mods/rp_bed/init.lua
@@ -506,7 +506,7 @@ minetest.register_node(
               local node = minetest.get_node(under)
               if placer and not placer:get_player_control().sneak then
                      if minetest.registered_nodes[node.name] and minetest.registered_nodes[node.name].on_rightclick then
-                            return minetest.registered_nodes[node.name].on_rightclick(pointed_thing.under, node, placer, itemstack) or itemstack
+                            return minetest.registered_nodes[node.name].on_rightclick(pointed_thing.under, node, placer, itemstack, pointed_thing) or itemstack
                      end
               end
 
