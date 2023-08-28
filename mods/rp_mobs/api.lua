8aba1759ad612e19b3a6b612826f633da071d120
Wuzzy <Wuzzy@disroot.org>
2023-08-28 13:44:51 +0200

    Add mob spawn item

--- mods/rp_mobs/api.lua
+++ mods/rp_mobs/api.lua
@@ -12,9 +12,47 @@ rp_mobs.register_mob = function(mobname, def)
 	minetest.register_entity(mobname, mdef.entity_definition)
 end
 
--- TODO
-rp_mobs.register_spawn_egg = function()
+rp_mobs.register_mob_item = function(mobname, invimg, desc)
+	local place
+	if not desc then
+		desc = rp_mobs.registered_mobs[mobname].description
+	end
+	minetest.register_craftitem(mobname, {
+		description = desc,
+		inventory_image = invimg,
+		groups = { spawn_egg = 1 },
+		on_place = function(itemstack, placer, pointed_thing)
+			local handled, handled_itemstack = util.on_place_pointed_node_handler(itemstack, placer, pointed_thing)
+			if handled then
+				return handled_itemstack
+			end
+			if pointed_thing.type == "node" then
+				local pos = pointed_thing.above
+				local pname = placer:get_player_name()
+				if minetest.is_protected(pos, pname) and
+						not minetest.check_player_privs(placer, "protection_bypass") then
+					 minetest.record_protection_violation(pos, pname)
+					 return itemstack
+				end
+
+				pos.y = pos.y + 0.5
+				local mob = minetest.add_entity(pos, mobname)
+				local ent = mob:get_luaentity()
+				if ent.type ~= "monster" then
+					-- set owner
+					ent.owner = pname
+					ent.tamed = true
+				end
+				 minetest.log("action", "[rp_mobs] "..pname.." spawns "..mobname.." at "..minetest.pos_to_string(pos, 1))
+				if not minetest.is_creative_enabled(pname) then
+					 itemstack:take_item()
+				end
+			end
+			return itemstack
+		end,
+	})
 end
+
 
 -- TODO
 rp_mobs.feed_tame = function()
--- mods/rp_mobs_mobs/boar.lua
+++ mods/rp_mobs_mobs/boar.lua
@@ -5,22 +5,20 @@ local S = minetest.get_translator("mobs")
 -- Changed to Boar and tweaked by KaadmY
 --
 rp_mobs.register_mob("rp_mobs_mobs:boar", {
-   description = S("Boar"),
-   entity_definition = {
-      hp_max = 20,
-      collisionbox = {-0.5, -1, -0.5, 0.5, 0.1, 0.5},
-      selectionbox = {-0.4, -1, -0.6, 0.4, 0.1, 0.7, rotate = true},
-      visual = "mesh",
-      mesh = "mobs_boar.x",
-      textures = { "mobs_boar.png" },
-      makes_footstep_sound = true,
-      on_rightclick = function(self, clicker)
-         rp_mobs.feed_tame(self, clicker, 8, true)
-         rp_mobs.capture_mob(self, clicker, 0, 5, 40, false, nil)
-      end,
-   },
+	description = S("Boar"),
+	entity_definition = {
+		hp_max = 20,
+		collisionbox = {-0.5, -1, -0.5, 0.5, 0.1, 0.5},
+		selectionbox = {-0.4, -1, -0.6, 0.4, 0.1, 0.7, rotate = true},
+		visual = "mesh",
+		mesh = "mobs_boar.x",
+		textures = { "mobs_boar.png" },
+		makes_footstep_sound = true,
+		on_rightclick = function(self, clicker)
+			rp_mobs.feed_tame(self, clicker, 8, true)
+			rp_mobs.capture_mob(self, clicker, 0, 5, 40, false, nil)
+		end,
+	},
 })
 
-
-
-
+rp_mobs.register_mob_item("rp_mobs_mobs:boar", "mobs_boar_inventory.png")
