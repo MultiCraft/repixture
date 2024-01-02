6445b3abf69d24c90c17fc2d5081b6f0db18a177
Wuzzy <Wuzzy@disroot.org>
2024-01-02 21:45:17 +0100

    Make paint bucket more user-friendly

--- mods/rp_paint/init.lua
+++ mods/rp_paint/init.lua
@@ -61,6 +61,9 @@ local facedir_color_map = {
 
 local BRUSH_USES = 100
 
+local BUCKET_HEIGHT_ABOVE_ZERO = 5/16
+local BUCKET_RADIUS = 6/16
+
 rp_paint.get_color = function(node)
 	local color
 	local def = minetest.registered_nodes[node.name]
@@ -233,7 +236,7 @@ minetest.register_tool("rp_paint:brush", {
 
 minetest.register_node("rp_paint:bucket", {
 	description = S("Paint Bucket"),
-	_tt_help = "Use to change paint color",
+	_tt_help = S("Use place key to change color").."\n"..S("Point at left/right part to get previous/next color"),
 
 	drawtype = "mesh",
 	mesh = "rp_default_bucket.obj",
@@ -255,7 +258,7 @@ minetest.register_node("rp_paint:bucket", {
 	is_ground_content = false,
 	selection_box = {
 		type = "fixed",
-		fixed = { -6/16, -0.5, -6/16, 6/16, 5/16, 6/16 },
+		fixed = { -BUCKET_RADIUS, -0.5, -BUCKET_RADIUS, BUCKET_RADIUS, BUCKET_HEIGHT_ABOVE_ZERO, BUCKET_RADIUS },
 	},
 	sounds = rp_sounds.node_sound_metal_defaults(),
 	walkable = false,
@@ -277,11 +280,89 @@ minetest.register_node("rp_paint:bucket", {
 		if not pointed_thing or util.handle_node_protection(clicker, pointed_thing) then
 			return
 		end
+		-- "direction" of color change (1 = next color, -1 = previous color)
+		local direction = 1
+		if clicker and clicker:is_player() then
+			local props = clicker:get_properties()
+			local eye_pos = clicker:get_pos()
+			eye_pos.y = eye_pos.y + props.eye_height
+			eye_pos = vector.add(eye_pos, clicker:get_eye_offset())
+			local lookdir = clicker:get_look_dir()
+			local handrange = minetest.registered_items[""].range
+			lookdir = vector.multiply(lookdir, handrange+1)
+			local look_pos = vector.add(eye_pos, lookdir)
+			-- do a raycast from the player to the look direction,
+			-- using the hand range + 1 as vector length.
+			-- (+1 serves as a small buffer)
+			-- With this method we can find the precise click location.
+			local rc = Raycast(eye_pos, look_pos, false, false)
+			local exact_pos
+			for rpt in rc do
+				if rpt.type == "node" then
+					local rptn = minetest.get_node(rpt.under)
+					if rptn.name == node.name then
+						exact_pos = rpt.intersection_point
+						break
+					end
+				end
+			end
+
+			local fine_pos = exact_pos
+			-- Fallback if raycast didn't find our paint bucket for some reason
+			if not fine_pos then
+				fine_pos = minetest.pointed_thing_to_face_pos(clicker, pointed_thing)
+				minetest.log("warning", "[rp_paint] "..clicker:get_player_name().." rightclicked paint bucket at "..minetest.pos_to_string(pos).." but the raycast failed to find it. Using less accurate fallback to find click position")
+			end
+			-- Depending on what was clicked and where the player stood, the paint bucket
+			-- will choose either the next or previous color.
+			-- Basically, if you click the left side of the face, the previous color
+			-- will be selected, and the right side gets you the next color.
+			-- The side textures should have subtle engraved small arrows
+			if pointed_thing.above.y ~= pointed_thing.under.y then
+				local cpos = clicker:get_pos()
+				local xdist = math.abs(pos.x-cpos.x)
+				local zdist = math.abs(pos.z-cpos.z)
+				if xdist > zdist then
+					if cpos.x < pos.x then
+						if fine_pos.z > pos.z then
+							direction = -1
+						end
+					else
+						if fine_pos.z < pos.z then
+							direction = -1
+						end
+					end
+				else
+					if cpos.z < pos.z then
+						if fine_pos.x < pos.x then
+							direction = -1
+						end
+					else
+						if fine_pos.x > pos.x then
+							direction = -1
+						end
+					end
+				end
+			else
+				if pointed_thing.above.z < pointed_thing.under.z and fine_pos.x < pos.x then
+					direction = -1
+				elseif pointed_thing.above.z > pointed_thing.under.z and fine_pos.x > pos.x then
+					direction = -1
+				elseif pointed_thing.above.x < pointed_thing.under.x and fine_pos.z > pos.z then
+					direction = -1
+				elseif pointed_thing.above.x > pointed_thing.under.x and fine_pos.z < pos.z then
+					direction = -1
+				end
+
+			end
+		end
 		local rot = node.param2 % 4
 		local color = bit.rshift(node.param2, 2)
-		color = color + 1
+		color = color + direction
 		if color >= rp_paint.COLOR_COUNT then
 			color = 0
+		elseif color < 0 then
+			color = rp_paint.COLOR_COUNT - 1
 		end
 		local meta = minetest.get_meta(pos)
 		meta:set_string("infotext", S("Paint Bucket (@1)", COLOR_NAMES[color+1]))
