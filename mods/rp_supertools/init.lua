c162a4dcd96c7571412c9d009af8ea83759c9cc7
Wuzzy <Wuzzy@disroot.org>
2024-03-11 14:20:40 +0100

    Add child tool; growth tool now grows mobs too

--- mods/rp_supertools/init.lua
+++ mods/rp_supertools/init.lua
@@ -1,5 +1,8 @@
 local S = minetest.get_translator("rp_supertools")
 
+local GROWTH_TOOL_USES = 13
+local CHILD_TOOL_USES = 13
+
 local grow_tall = function(pos, y_dir, nodename)
     local newpos
     for i=1, 10 do
@@ -19,11 +22,27 @@ minetest.register_craftitem(
    "rp_supertools:growth_tool",
    {
       description = S("Growth Tool"),
-      _tt_help = S("Make plants grow instantly"),
+      _tt_help = S("Make plants and mobs grow instantly"),
       inventory_image = "rp_supertools_growth_tool.png",
       wield_image = "rp_supertools_growth_tool.png",
       groups = { supertool = 1, tool = 1 },
       stack_max = 1,
+      on_secondary_use = function(itemstack, placer, pointed_thing)
+	 -- Handle growing child mobs into adults
+         if pointed_thing.type == "object" then
+            local obj = pointed_thing.ref
+            local ent = obj:get_luaentity()
+            if ent and ent._cmi_is_mob and ent._child then
+               rp_mobs.turn_into_adult(ent)
+               if not minetest.is_creative_enabled(placer:get_player_name()) then
+                  itemstack:add_wear_by_uses(GROWTH_TOOL_USES)
+               end
+               local pos = obj:get_pos()
+               minetest.log("action", "[rp_supertools] " .. placer:get_player_name() .. " used groth tool on mob '"..ent.name.."' at "..minetest.pos_to_string(pos, 1))
+            end
+            return itemstack
+         end
+      end,
       on_place = function(itemstack, placer, pointed_thing)
          -- Handle pointed node handlers and protection
          local handled, handled_itemstack = util.on_place_pointed_node_handler(itemstack, placer, pointed_thing)
@@ -34,7 +53,11 @@ minetest.register_craftitem(
             return itemstack
          end
 
-	 -- Handle growing things
+         if pointed_thing.type ~= "node" then
+            return itemstack
+         end
+
+	 -- Handle growing nodes
 	 local apos = pointed_thing.above
 	 local upos = pointed_thing.under
          local unode = minetest.get_node(upos)
@@ -116,12 +139,39 @@ minetest.register_craftitem(
 	 if used then
             minetest.sound_play({name="rp_farming_place_nonseed", gain=0.75}, {pos=pointed_thing.under}, true)
             if not minetest.is_creative_enabled(placer:get_player_name()) then
-               itemstack:add_wear_by_uses(13)
+               itemstack:add_wear_by_uses(GROWTH_TOOL_USES)
             end
 
             minetest.log("action", "[rp_supertools] " .. placer:get_player_name() .. " used growth tool on "..unode.name.." at "..minetest.pos_to_string(upos))
          end
 
+         return itemstack
+      end,
+})
+
+minetest.register_craftitem(
+   "rp_supertools:child_tool",
+   {
+      description = S("Child Tool"),
+      _tt_help = S("Turn mobs into children"),
+      inventory_image = "rp_supertools_child_tool.png",
+      wield_image = "rp_supertools_child_tool.png",
+      groups = { supertool = 1, tool = 1 },
+      stack_max = 1,
+      on_secondary_use = function(itemstack, placer, pointed_thing)
+         if pointed_thing.type ~= "object" then
+           return itemstack
+         end
+         local obj = pointed_thing.ref
+         local ent = obj:get_luaentity()
+         if ent and ent._cmi_is_mob and not ent._child then
+            local pos = obj:get_pos()
+            rp_mobs.turn_into_child(obj)
+            if not minetest.is_creative_enabled(placer:get_player_name()) then
+               itemstack:add_wear_by_uses(CHILD_TOOL_USES)
+            end
+            minetest.log("action", "[rp_supertools] " .. placer:get_player_name() .. " used child tool on '"..ent.name.."' at "..minetest.pos_to_string(pos, 1))
+         end
          return itemstack
       end,
 })
